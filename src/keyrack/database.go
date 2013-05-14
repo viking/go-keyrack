708a29e60e123fa926a0e77d6d0a3de703f5e861
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-05-14 14:51:34 -0500

    Add Login and LoginList (which implements sort.Interface)

---
+++ src/keyrack/database.go
@@ -0,0 +1,30 @@
+package keyrack
+
+type Login struct {
+  Id uint64
+  Site string
+  Username string
+  Password string
+}
+
+type LoginList []*Login
+
+func (list LoginList) Len() int {
+  return len([]*Login(list))
+}
+
+func (list LoginList) Less(i, j int) bool {
+  if list[i].Site == list[j].Site {
+    if list[i].Username == list[j].Username {
+      return list[i].Id < list[j].Id
+    } else {
+      return list[i].Username < list[j].Username
+    }
+  } else {
+    return list[i].Site < list[j].Site
+  }
+}
+
+func (list LoginList) Swap(i, j int) {
+  list[i], list[j] = list[j], list[i]
+}
