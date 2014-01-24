f3f8979ec65f1c2c36f92522b9c7839bab2d1e10
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-01-24 15:58:34 -0600

    Create public error vars for a couple of errors

--- database.go
+++ database.go
@@ -1,145 +1,147 @@
 package keyrack
 
 import (
-  "os"
-  "io"
-  "io/ioutil"
-  "fmt"
-  "sync"
-  "encoding/json"
-  "crypto/rand"
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"sync"
 )
 
+var ErrInvalidPassword = fmt.Errorf("invalid password")
+
 type Database struct {
-  Version uint8
-  Data *Secret
-  private struct {
-    Top *Group
-    Key []byte
-  }
-  mutex sync.Mutex
+	Version uint8
+	Data    *Secret
+	private struct {
+		Top *Group
+		Key []byte
+	}
+	mutex sync.Mutex
 }
 
 func NewDatabase() (db *Database, err error) {
-  db = &Database{Version: 1}
+	db = &Database{Version: 1}
 
-  var n int
-  db.private.Key = make([]byte, 32)
-  n, err = io.ReadFull(rand.Reader, db.private.Key)
-  if err != nil {
-    return
-  }
-  if n != len(db.private.Key) {
-    err = fmt.Errorf("couldn't generate key")
-    return
-  }
+	var n int
+	db.private.Key = make([]byte, 32)
+	n, err = io.ReadFull(rand.Reader, db.private.Key)
+	if err != nil {
+		return
+	}
+	if n != len(db.private.Key) {
+		err = fmt.Errorf("couldn't generate key")
+		return
+	}
 
-  db.private.Top = NewGroup("Top")
-  return
+	db.private.Top = NewGroup("Top")
+	return
 }
 
 func LoadDatabase(filename string, password []byte) (db *Database, err error) {
-  var (
-    f *os.File
-    dbJSON, privateJSON []byte
-  )
+	var (
+		f                   *os.File
+		dbJSON, privateJSON []byte
+	)
 
-  f, err = os.Open(filename)
-  if err != nil {
-    return
-  }
-  defer f.Close()
+	f, err = os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
 
-  dbJSON, err = ioutil.ReadAll(f)
-  if err != nil {
-    return
-  }
-  db = new(Database)
-  err = json.Unmarshal(dbJSON, db)
-  if err != nil {
-    return
-  }
-  privateJSON, err = db.Data.Message(password)
-  if err != nil {
-    return
-  }
-  err = json.Unmarshal(privateJSON, &db.private)
+	dbJSON, err = ioutil.ReadAll(f)
+	if err != nil {
+		return
+	}
+	db = new(Database)
+	err = json.Unmarshal(dbJSON, db)
+	if err != nil {
+		return
+	}
+	privateJSON, err = db.Data.Message(password)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(privateJSON, &db.private)
 
-  return
+	return
 }
 
 func (db *Database) Key() []byte {
-  return db.private.Key
+	return db.private.Key
 }
 
 func (db *Database) Top() *Group {
-  return db.private.Top
+	return db.private.Top
 }
 
 func (db *Database) Save(filename string, password []byte) (err error) {
-  db.mutex.Lock()
-  defer db.mutex.Unlock()
+	db.mutex.Lock()
+	defer db.mutex.Unlock()
 
-  if db.Data != nil && !db.Data.IsPasswordValid(password) {
-    err = fmt.Errorf("invalid password")
-    return
-  }
+	if db.Data != nil && !db.Data.IsPasswordValid(password) {
+		err = ErrInvalidPassword
+		return
+	}
 
-  /* Encrypt all the logins */
-  db.encryptLogins(db.private.Top)
+	/* Encrypt all the logins */
+	db.encryptLogins(db.private.Top)
 
-  /* Serialize group to JSON and encrypt */
-  var privateJSON []byte
-  privateJSON, err = json.Marshal(db.private)
-  if err != nil {
-    return
-  }
+	/* Serialize group to JSON and encrypt */
+	var privateJSON []byte
+	privateJSON, err = json.Marshal(db.private)
+	if err != nil {
+		return
+	}
 
-  db.Data, err = NewSecret(privateJSON, password)
-  if err != nil {
-    return
-  }
+	db.Data, err = NewSecret(privateJSON, password)
+	if err != nil {
+		return
+	}
 
-  /* Serialize database to JSON */
-  var dbJSON []byte
-  dbJSON, err = json.Marshal(db)
-  if err != nil {
-    return
-  }
+	/* Serialize database to JSON */
+	var dbJSON []byte
+	dbJSON, err = json.Marshal(db)
+	if err != nil {
+		return
+	}
 
-  /* Write to the file */
-  var f *os.File
-  f, err = os.Create(filename)
-  if err != nil {
-    return
-  }
-  defer f.Close()
+	/* Write to the file */
+	var f *os.File
+	f, err = os.Create(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
 
-  var n int
-  n, err = f.Write(dbJSON)
-  if err != nil {
-    return
-  }
-  if n != len(dbJSON) {
-    err = fmt.Errorf("couldn't write to file")
-    return
-  }
+	var n int
+	n, err = f.Write(dbJSON)
+	if err != nil {
+		return
+	}
+	if n != len(dbJSON) {
+		err = fmt.Errorf("couldn't write to file")
+		return
+	}
 
-  return
+	return
 }
 
 func (db *Database) encryptLogins(group *Group) (err error) {
-  for _, login := range group.Logins {
-    err = login.Encrypt(db.private.Key)
-    if err != nil {
-      return
-    }
-  }
-  for _, subgroup := range group.Groups {
-    err = db.encryptLogins(subgroup)
-    if err != nil {
-      return
-    }
-  }
-  return
+	for _, login := range group.Logins {
+		err = login.Encrypt(db.private.Key)
+		if err != nil {
+			return
+		}
+	}
+	for _, subgroup := range group.Groups {
+		err = db.encryptLogins(subgroup)
+		if err != nil {
+			return
+		}
+	}
+	return
 }
--- group.go
+++ group.go
@@ -3,59 +3,61 @@ package keyrack
 import "fmt"
 
 type Group struct {
-  Name string
-  Logins LoginList
-  Groups GroupList
+	Name   string
+	Logins LoginList
+	Groups GroupList
 }
 
+var ErrLoginExists = fmt.Errorf("there is already a login with the same site/username")
+
 func NewGroup(name string) (group *Group) {
-  group = &Group{Name: name}
-  group.Logins = make(LoginList, 0)
-  group.Groups = make(GroupList, 0)
-  return
+	group = &Group{Name: name}
+	group.Logins = make(LoginList, 0)
+	group.Groups = make(GroupList, 0)
+	return
 }
 
 func (group *Group) AddLogin(site, username, password string) (err error) {
-  var login *Login
+	var login *Login
 
-  // First check to see if there is another login with the same site/username
-  for _, login = range group.Logins {
-    if login.Site == site && login.Username == username {
-      err = fmt.Errorf("there is already a login with the same site/username")
-      return
-    }
-  }
+	// First check to see if there is another login with the same site/username
+	for _, login = range group.Logins {
+		if login.Site == site && login.Username == username {
+			err = ErrLoginExists
+			return
+		}
+	}
 
-  login = NewLogin(site, username, password)
-  group.Logins = append(group.Logins, login)
-  return
+	login = NewLogin(site, username, password)
+	group.Logins = append(group.Logins, login)
+	return
 }
 
 func (group *Group) AddGroup(name string) (err error) {
-  var subgroup *Group
+	var subgroup *Group
 
-  for _, subgroup = range group.Groups {
-    if subgroup.Name == name {
-      err = fmt.Errorf("there is already a group with the same name")
-      return
-    }
-  }
+	for _, subgroup = range group.Groups {
+		if subgroup.Name == name {
+			err = fmt.Errorf("there is already a group with the same name")
+			return
+		}
+	}
 
-  subgroup = NewGroup(name)
-  group.Groups = append(group.Groups, subgroup)
-  return
+	subgroup = NewGroup(name)
+	group.Groups = append(group.Groups, subgroup)
+	return
 }
 
 type GroupList []*Group
 
 func (list GroupList) Len() int {
-  return len(list)
+	return len(list)
 }
 
 func (list GroupList) Less(i, j int) bool {
-  return list[i].Name < list[j].Name
+	return list[i].Name < list[j].Name
 }
 
 func (list GroupList) Swap(i, j int) {
-  list[i], list[j] = list[j], list[i]
+	list[i], list[j] = list[j], list[i]
 }
