39d618ee8a6537b194d049b0b303934cc07f2520
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2014-01-24 17:42:39 -0600

    Add group navigation, group creation, and password printing

--- keyrack/ui.go
+++ keyrack/ui.go
@@ -5,6 +5,7 @@ import (
 	"github.com/howeyc/gopass"
 	"keyrack"
 	"math"
+	"strconv"
 )
 
 // return the character width of the maximum index
@@ -36,10 +37,10 @@ func printMenu(group *keyrack.Group) {
 		fmt.Printf("% *s. %s\n", width, index, subgroup.Name)
 	}
 	for i, login := range group.Logins {
-		index := fmt.Sprintf("S%d", i+1)
+		index := fmt.Sprintf("L%d", i+1)
 		fmt.Printf("% *s. %s [%s]\n", width, index, login.Site, login.Username)
 	}
-	fmt.Println("Commands: new save quit")
+	fmt.Println("Commands: login group up save quit")
 }
 
 // read input from user
@@ -53,6 +54,11 @@ func getInput(prompt string) (command string, err error) {
 func getPassword() []byte {
 	fmt.Printf("Password: ")
 	return gopass.GetPasswd()
+}
+
+// print password from login
+func printPassword(password string) {
+	fmt.Println(password)
 }
 
 // add login to group
@@ -84,11 +90,32 @@ func newLogin(group *keyrack.Group) (err error) {
 	return
 }
 
+// add subgroup to group
+func newGroup(group *keyrack.Group) (err error) {
+	var name string
+
+	name, err = getInput("Name:")
+	if err != nil || name == "" {
+		return
+	}
+
+	err = group.AddGroup(name)
+	if err != nil {
+		if err == keyrack.ErrGroupExists {
+			fmt.Println("Error:", err)
+			err = nil
+		}
+	}
+	return
+}
+
+// main menu
 func menu(session *Session, group *keyrack.Group) (quit bool, err error) {
 	for !quit && err == nil {
 		printMenu(group)
 
-		for ok := false; !ok; {
+		up := false
+		for ok := false; !ok && err == nil; {
 			var command string
 			command, err = getInput("?")
 			if err != nil {
@@ -97,8 +124,14 @@ func menu(session *Session, group *keyrack.Group) (quit bool, err error) {
 
 			ok = true
 			switch command {
-			case "new":
+			case "login":
 				err = newLogin(group)
+
+			case "group":
+				err = newGroup(group)
+
+			case "up":
+				up = true
 
 			case "save":
 				password := getPassword()
@@ -114,8 +147,36 @@ func menu(session *Session, group *keyrack.Group) (quit bool, err error) {
 				quit = true
 
 			default:
-				ok = false
+				var i int
+				switch command[0] {
+				case 'G', 'g':
+					numGroups := len(group.Groups)
+					i, err = strconv.Atoi(command[1:])
+					if ok = err == nil && numGroups > 0 && i > 0 && i <= numGroups; ok {
+						subgroup := group.Groups[i-1]
+						quit, err = menu(session, subgroup)
+					}
+
+				case 'L', 'l':
+					numLogins := len(group.Logins)
+					i, err = strconv.Atoi(command[1:])
+					if ok = err == nil && numLogins > 0 && i > 0 && i <= numLogins; ok {
+						login := group.Logins[i-1]
+						err = session.db.DecryptLogin(login)
+						if err == nil {
+							printPassword(login.Password())
+							login.Clear()
+						}
+					}
+
+				default:
+					ok = false
+				}
 			}
+		}
+
+		if up && session.db.Top() != group {
+			break
 		}
 	}
 	return
