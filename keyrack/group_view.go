b16b32ea1b38eca2bc70ab5d2a74ea5cb5963349
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2015-08-26 11:59:50 -0500

    Create GroupView

---
+++ keyrack/group_view.go
@@ -0,0 +1,58 @@
+package keyrack
+
+import (
+	"bytes"
+	"text/template"
+)
+
+var GroupTemplate string = "Group {{.Name}}\n\n{{range .Groups}}G{{.Index}}. {{.Name}}\n{{end}}{{range .Logins}}L{{.Index}}. {{.Site}} ({{.Username}})\n{{end}}"
+
+type GroupView struct {
+	Group *Group
+}
+
+func (gv GroupView) Name() string {
+	return gv.Group.Name
+}
+
+type GroupViewGroup struct {
+	Index int
+	Name  string
+}
+
+func (gv GroupView) Groups() (result []GroupViewGroup) {
+	for i, group := range gv.Group.Groups {
+		result = append(result, GroupViewGroup{i, group.Name})
+	}
+	return
+}
+
+type GroupViewLogin struct {
+	Index    int
+	Site     string
+	Username string
+}
+
+func (gv GroupView) Logins() (result []GroupViewLogin) {
+	for i, login := range gv.Group.Logins {
+		result = append(result, GroupViewLogin{i, login.Site, login.Username})
+	}
+	return
+}
+
+func (gv GroupView) Render() (result string, err error) {
+	tmpl, err := template.New("group").Parse(GroupTemplate)
+	if err != nil {
+		return
+	}
+
+	var buf []byte
+	writer := bytes.NewBuffer(buf)
+	err = tmpl.Execute(writer, gv)
+	if err != nil {
+		return
+	}
+
+	result = string(writer.Bytes())
+	return
+}
---
+++ keyrack/group_view_test.go
@@ -0,0 +1,37 @@
+package keyrack
+
+import (
+	"testing"
+)
+
+func TestGroupView_Render_NoLogins(t *testing.T) {
+	group := NewGroup("blah")
+	group.AddGroup("foo")
+	group.AddGroup("bar")
+	view := GroupView{group}
+	result, err := view.Render()
+	if err == nil {
+		expected := "Group blah\n\nG0. foo\nG1. bar\n"
+		if result != expected {
+			t.Errorf("expected <%+v>, got <%+v>", []byte(expected), []byte(result))
+		}
+	} else {
+		t.Error(err)
+	}
+}
+
+func TestGroupView_Render_NoGroups(t *testing.T) {
+	group := NewGroup("blah")
+	group.AddLogin("foo", "bro", "secret")
+	group.AddLogin("bar", "dude", "secret")
+	view := GroupView{group}
+	result, err := view.Render()
+	if err == nil {
+		expected := "Group blah\n\nL0. foo (bro)\nL1. bar (dude)\n"
+		if result != expected {
+			t.Errorf("expected <%+v>, got <%+v>", []byte(expected), []byte(result))
+		}
+	} else {
+		t.Error(err)
+	}
+}
